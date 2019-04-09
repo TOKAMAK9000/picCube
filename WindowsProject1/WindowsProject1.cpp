487ed7c60f9516b99cd3209ebef4dcbe8f7c66ce
李家丞 <tokamak9000@gmail.com>
2019-04-09 15:08:45 +0800

    上传资源文件

--- WindowsProject1/WindowsProject1.cpp
+++ WindowsProject1/WindowsProject1.cpp
@@ -1,17 +1,14 @@
 ﻿#include "stdafx.h"
-// include the basic windows header files and the Direct3D header file
 #include <windows.h>
 #include <windowsx.h>
 #include <d3d9.h>
 #include <d3dx9.h>
 
-// define the screen resolution and keyboard macros
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
 #define KEY_DOWN(vk_code) ((GetAsyncKeyState(vk_code) & 0x8000) ? 1 : 0)
 #define KEY_UP(vk_code) ((GetAsyncKeyState(vk_code) & 0x8000) ? 0 : 1)
 
-// include the Direct3D Library files
 #pragma comment (lib, "d3d9.lib")
 #pragma comment (lib, "d3dx9.lib")
 
@@ -20,23 +17,20 @@ LPDIRECT3D9 d3d;
 LPDIRECT3DDEVICE9 d3ddev;
 LPDIRECT3DVERTEXBUFFER9 v_buffer = NULL;
 LPDIRECT3DINDEXBUFFER9 i_buffer = NULL;
-LPDIRECT3DTEXTURE9 texture = NULL;    // our first texture
+LPDIRECT3DTEXTURE9 texture = NULL; 
 
-// function prototypes
 void initD3D(HWND hWnd);
 void render_frame(void);
 void cleanD3D(void);
 void init_graphics(void);
-void init_light(void);    // sets up the light and the material
+void init_light(void); 
 
 struct CUSTOMVERTEX { FLOAT X, Y, Z; D3DVECTOR NORMAL; FLOAT U, V; };
 #define CUSTOMFVF (D3DFVF_XYZ | D3DFVF_NORMAL | D3DFVF_TEX1)
 
-// the WindowProc function prototype
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
 
-// the entry point for any Windows program
 int WINAPI WinMain(HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
 	LPSTR lpCmdLine,
@@ -62,10 +56,7 @@ int WINAPI WinMain(HINSTANCE hInstance,
 
 	ShowWindow(hWnd, nCmdShow);
 
-	// set up and initialize Direct3D
 	initD3D(hWnd);
-
-	// enter the main loop:
 
 	MSG msg;
 
@@ -86,14 +77,12 @@ int WINAPI WinMain(HINSTANCE hInstance,
 			PostMessage(hWnd, WM_DESTROY, 0, 0);
 	}
 
-	// clean up DirectX and COM
 	cleanD3D();
 
 	return msg.wParam;
 }
 
 
-// this is the main message handler for the program
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	switch (message)
@@ -109,7 +98,6 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
 }
 
 
-// this function initializes and prepares Direct3D for use
 void initD3D(HWND hWnd)
 {
 	d3d = Direct3DCreate9(D3D_SDK_VERSION);
@@ -126,7 +114,6 @@ void initD3D(HWND hWnd)
 	d3dpp.EnableAutoDepthStencil = TRUE;
 	d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
 
-	// create a device class using this information and the info from the d3dpp stuct
 	d3d->CreateDevice(D3DADAPTER_DEFAULT,
 		D3DDEVTYPE_HAL,
 		hWnd,
@@ -134,16 +121,15 @@ void initD3D(HWND hWnd)
 		&d3dpp,
 		&d3ddev);
 
-	init_graphics();    // call the function to initialize the cube
-	init_light();    // call the function to initialize the light and material
+	init_graphics();  
+	init_light(); 
 
-	d3ddev->SetRenderState(D3DRS_LIGHTING, TRUE);    // turn on the 3D lighting
-	d3ddev->SetRenderState(D3DRS_ZENABLE, TRUE);    // turn on the z-buffer
-	d3ddev->SetRenderState(D3DRS_AMBIENT, D3DCOLOR_XRGB(50, 50, 50));    // ambient light
+	d3ddev->SetRenderState(D3DRS_LIGHTING, TRUE); 
+	d3ddev->SetRenderState(D3DRS_ZENABLE, TRUE); 
+	d3ddev->SetRenderState(D3DRS_AMBIENT, D3DCOLOR_XRGB(50, 50, 50)); 
 }
 
 
-// this is the function used to render a single frame
 void render_frame(void)
 {
 	d3ddev->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
@@ -151,40 +137,33 @@ void render_frame(void)
 
 	d3ddev->BeginScene();
 
-	// select which vertex format we are using
 	d3ddev->SetFVF(CUSTOMFVF);
 
-	// set the view transform
 	D3DXMATRIX matView;
 	D3DXMatrixLookAtLH(&matView,
-		&D3DXVECTOR3(0.0f, 8.0f, 25.0f),    // the camera position
-		&D3DXVECTOR3(0.0f, 0.0f, 0.0f),      // the look-at position
-		&D3DXVECTOR3(0.0f, 1.0f, 0.0f));    // the up direction
+		&D3DXVECTOR3(0.0f, 8.0f, 25.0f), 
+		&D3DXVECTOR3(0.0f, 0.0f, 0.0f), 
+		&D3DXVECTOR3(0.0f, 1.0f, 0.0f));  
 	d3ddev->SetTransform(D3DTS_VIEW, &matView);
 
-	// set the projection transform
 	D3DXMATRIX matProjection;
 	D3DXMatrixPerspectiveFovLH(&matProjection,
 		D3DXToRadian(25),
 		(FLOAT)SCREEN_WIDTH / (FLOAT)SCREEN_HEIGHT,
-		1.0f,    // the near view-plane
-		100.0f);    // the far view-plane
+		1.0f,   
+		100.0f);  
 	d3ddev->SetTransform(D3DTS_PROJECTION, &matProjection);
 
-	// set the world transform
 	static float index = 0.0f; index += 0.015f;
 	D3DXMATRIX matRotateY;
 	D3DXMatrixRotationY(&matRotateY, index);
 	d3ddev->SetTransform(D3DTS_WORLD, &(matRotateY));
 
-	// select the vertex and index buffers to use
 	d3ddev->SetStreamSource(0, v_buffer, 0, sizeof(CUSTOMVERTEX));
 	d3ddev->SetIndices(i_buffer);
 
-	// set the texture
 	d3ddev->SetTexture(0, texture);
 
-	// draw the cube
 	d3ddev->DrawIndexedPrimitive(D3DPT_TRIANGLELIST, 0, 0, 24, 0, 12);
 
 	d3ddev->EndScene();
@@ -193,25 +172,22 @@ void render_frame(void)
 }
 
 
-// this is the function that cleans up Direct3D and COM
 void cleanD3D(void)
 {
 	v_buffer->Release();
 	i_buffer->Release();
-	texture->Release();    // close and release the texture
+	texture->Release(); 
 	d3ddev->Release();
 	d3d->Release();
 }
 
 
-// this is the function that puts the 3D models into video RAM
 void init_graphics(void)
 {
-	D3DXCreateTextureFromFile(d3ddev,    // the Direct3D device
-		L"C:\\Users\\TOKAMAK\\Desktop\\new\\image.png",    // the filename of the texture
-		&texture);    // the address of the texture storage
+	D3DXCreateTextureFromFile(d3ddev,   
+		L"C:\\Users\\TOKAMAK\\Desktop\\new\\image.png",  
+		&texture);   
 
-// create the vertices using the CUSTOMVERTEX struct
 	CUSTOMVERTEX vertices[] =
 	{
 		{ -3.0f, -3.0f, 3.0f,  0.0f, 0.0f, 1.0f,  0.0f, 0.0f, },    // side 1
@@ -245,7 +221,6 @@ void init_graphics(void)
 		{ -3.0f, 3.0f, 3.0f,  -1.0f, 0.0f, 0.0f,  1.0f, 1.0f, },
 	};
 
-	// create a vertex buffer interface called v_buffer
 	d3ddev->CreateVertexBuffer(24 * sizeof(CUSTOMVERTEX),
 		0,
 		CUSTOMFVF,
@@ -253,7 +228,7 @@ void init_graphics(void)
 		&v_buffer,
 		NULL);
 
-	VOID* pVoid;    // a void pointer
+	VOID* pVoid;  
 
 	// lock v_buffer and load the vertices into it
 	v_buffer->Lock(0, 0, (void**)&pVoid, 0);
@@ -277,7 +252,6 @@ void init_graphics(void)
 		22, 21, 23,
 	};
 
-	// create an index buffer interface called i_buffer
 	d3ddev->CreateIndexBuffer(36 * sizeof(short),
 		0,
 		D3DFMT_INDEX16,
@@ -285,30 +259,28 @@ void init_graphics(void)
 		&i_buffer,
 		NULL);
 
-	// lock i_buffer and load the indices into it
 	i_buffer->Lock(0, 0, (void**)&pVoid, 0);
 	memcpy(pVoid, indices, sizeof(indices));
 	i_buffer->Unlock();
 }
 
 
-// this is the function that sets up the lights and materials
 void init_light(void)
 {
-	D3DLIGHT9 light;    // create the light struct
-	D3DMATERIAL9 material;    // create the material struct
+	D3DLIGHT9 light;  
+	D3DMATERIAL9 material; 
 
-	ZeroMemory(&light, sizeof(light));    // clear out the light struct for use
-	light.Type = D3DLIGHT_DIRECTIONAL;    // make the light type 'directional light'
-	light.Diffuse = D3DXCOLOR(0.5f, 0.5f, 0.5f, 1.0f);    // set the light's color
+	ZeroMemory(&light, sizeof(light)); 
+	light.Type = D3DLIGHT_DIRECTIONAL;
+	light.Diffuse = D3DXCOLOR(0.5f, 0.5f, 0.5f, 1.0f); 
 	light.Direction = D3DXVECTOR3(-1.0f, -0.3f, -1.0f);
 
-	d3ddev->SetLight(0, &light);    // send the light struct properties to light #0
-	d3ddev->LightEnable(0, TRUE);    // turn on light #0
+	d3ddev->SetLight(0, &light);  
+	d3ddev->LightEnable(0, TRUE);   
 
-	ZeroMemory(&material, sizeof(D3DMATERIAL9));    // clear out the struct for use
-	material.Diffuse = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);    // set diffuse color to white
-	material.Ambient = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);    // set ambient color to white
+	ZeroMemory(&material, sizeof(D3DMATERIAL9));  
+	material.Diffuse = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f); 
+	material.Ambient = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f); 
 
-	d3ddev->SetMaterial(&material);    // set the globably-used material to &material
+	d3ddev->SetMaterial(&material); 
 }
